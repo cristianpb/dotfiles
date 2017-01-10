c97fdb5aab4b36a7299f6c421073343ceaacc1ab
Cris <felipebrokate@gmail.com>
2017-01-10 23:27:35 +0100

    Getpass password

---
+++ offlineimap.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python2
+# executes gnome-keyring-query get passwd
+# and returns the output
+
+import locale
+from subprocess import Popen, PIPE
+
+encoding = locale.getdefaultlocale()[1]
+
+def get_password(p):
+    (out, err) = Popen(["gnome-keyring-query", "get", p], stdout=PIPE).communicate()
+    return out.decode(encoding).strip()
