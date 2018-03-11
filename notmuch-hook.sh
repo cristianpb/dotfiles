7d66e134a35f360255b20922522e058af05be5e7
cristianpb <felipebrokate@gmail.com>
2018-03-11 21:41:23 +0100

    Add audio mute to i3 and fix polybar icons

--- notmuch-hook.sh
+++ notmuch-hook.sh
@@ -3,6 +3,6 @@ notmuch new
 # retag all "new" messages "inbox" and "unread"
 notmuch tag +inbox +unread -new -- tag:new
 # tag all messages from "me" as sent and remove tags inbox and unread
-notmuch tag -new -inbox +sent -- from:felipebrokate@gmail.com or from:cristian.perez-brokate@chimie-paristech.fr or from:cperez@kernix.com
+notmuch tag -new -inbox +sent -- from:felipebrokate@gmail.com or from:cperez@kernix.com
 # tag newsletters, but dont show them in inbox
 notmuch tag +newsletters +unread -new -inbox -- from:newsletter@example.org or subject:'*ewsletter*'
