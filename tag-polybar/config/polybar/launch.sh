6e2c4121f4be4e68c4a3d1bea46fd6fad49ee1f0
cristianpb <18708179+cristianpb@users.noreply.github.com>
2019-02-05 23:23:41 +0100

    Add polybar/i3 multiple monitor support

--- tag-polybar/config/polybar/launch.sh
+++ tag-polybar/config/polybar/launch.sh
@@ -7,7 +7,9 @@ killall -q polybar
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar1 and bar2
-polybar top &
-polybar bottom &
+for m in $(polybar --list-monitors | cut -d":" -f1); do
+    MONITOR=$m polybar --reload top &
+    MONITOR=$m polybar --reload bottom &
+done
 
 echo "Bars launched..."
