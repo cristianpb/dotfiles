b11c9f579cf64c081d4957a02ee3189118c55f8c
cristianpb <felipebrokate@gmail.com>
2019-02-09 08:12:28 +0100

    Update xrandr rules for Dual monitor

--- DualMonitor.sh
+++ DualMonitor.sh
@@ -3,9 +3,9 @@
 # set up the two monitors for bspwm
 # NOTE This is a simplistic approach because I already know the settings I
 # want to apply.
-my_laptop_external_monitor=$(xrandr --query | grep 'VGA1')
+my_laptop_external_monitor=$(xrandr --query | grep 'DP-1-3')
 if [[ $my_laptop_external_monitor = *connected* ]]; then
-    xrandr --output eDP1 --primary --mode 1366x768 --rotate normal --output VGA1 --mode 1920x1080 --rotate normal --right-of eDP1
+    xrandr --output eDP-1 --primary --mode 1366x768 --pos 192x1200 --rotate normal --output DP-1-1 --off --output HDMI-1 --off --output DP-1-3 --mode 1920x1200 --pos 0x0 --rotate normal --output DP-1-2 --off --output DP-1 --off --output HDMI-2 --off
 else
-    xrandr --output eDP1 --primary --mode 1366x768 --rotate normal
+    xrandr --output eDP-1 --primary --mode 1366x768 --pos 0x0 --rotate normal --output DP-1-1 --off --output HDMI-1 --off --output DP-1-3 --off --output DP-1-2 --off --output DP-1 --off --output HDMI-2 --off
 fi
