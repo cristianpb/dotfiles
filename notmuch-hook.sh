13fb22d4880eac3413d300379405bc9c63bc3ea7
cristianpb <felipebrokate@gmail.com>
2019-07-05 23:51:10 +0200

    Remove create links script

--- notmuch-hook.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-notmuch new
-# retag all "new" messages "inbox" and "unread"
-notmuch tag +inbox +unread -new -- tag:new
-# tag all messages from "me" as sent and remove tags inbox and unread
-notmuch tag -new -inbox +sent -- from:felipebrokate@gmail.com or from:cperez@kernix.com
-# tag newsletters, but dont show them in inbox
-notmuch tag +newsletters +unread -new -inbox -- from:newsletter@example.org or subject:'*ewsletter*'
