e8dae16609be16541b7c9524fd5aaa22fbb6ba38
cristianpb <felipebrokate@gmail.com>
2020-04-20 22:56:25 +0200

    🎨 Improve bing script

--- BingDownloadImage.sh
+++ BingDownloadImage.sh
@@ -3,15 +3,16 @@
 #Resolution options: 1366×768, 1920×1080, 1920×1200
 Market="en-US"
 Resolution="1920x1080"
-Directory="$HOME/Images/Wallpaper/"
+Directory="$HOME/Pictures/Wallpaper/"
 FileName="wallpaper.jpg"
 FileNamepng="wallpaper.png"
 
-#mkdir -pv "$Directory"
+if [ ! -f /tmp/foo.txt ]; then
+    echo "Creating folder"; mkdir -pv "$Directory";
+fi
 
 URL=($(curl -s 'http://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt='"$Market" | python -c "import json,sys;obj=json.load(sys.stdin);print(obj['images'][0]['urlbase']);"))
 ImageURL="http://www.bing.com"/$URL"_"$Resolution".jpg"
 echo "Downloading Bing image to: $Directory"
 curl -so "$Directory"/$FileName "$ImageURL"
 convert "$Directory"/$FileName "$Directory"/$FileNamepng
-rm $FileName 
