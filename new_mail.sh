824454965d42fe3263020f4627677b11d12cb312
cristianpb <18708179+cristianpb@users.noreply.github.com>
2021-10-05 00:10:15 +0200

    🎨 Use home instead of custom name

--- new_mail.sh
+++ new_mail.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-countG=$(ls /home/arch/.mail/gmail/Inbox/new | wc -l)
-countM=$(ls /home/arch/.mail/ministere/Inbox/new | wc -l)
+countG=$(ls $HOME/.mail/gmail/Inbox/new | wc -l)
 count=$((countG + countM))
 
 if [[ -n "$count" && "$count" -gt 0 ]]; then
